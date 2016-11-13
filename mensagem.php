101dfd5fbee9a5e2dff4f0ef8f834e50e9949771
AlineDominique <alinedomi.silva@gmail.com>
2016-11-13 18:35:23 -0200

    Primeiro_Commit

---
+++ mensagem.php
@@ -0,0 +1,44 @@
+<?php
+function mensagens($codigo){
+	$mensagens = array(
+					array(
+						"Codigo"=>1
+						,"Mensagem"=>"Caminho especificado nao encontrado"
+					)
+					,array(
+						"Codigo"=>2
+						,"Mensagem"=>"Nenhum dado recebido"
+					)
+					,array(
+						"Codigo"=>3
+						,"Mensagem"=>"Estrutura de dados diferente do esperado"
+					)
+					,array(
+						"Codigo"=>4	
+						,"Mensagem"=>"Inserido com sucesso!"
+					)
+					,array(
+						"Codigo"=>5	
+						,"Mensagem"=>"Código inválido"
+					)
+					
+					,array(
+						"Codigo"=>6	
+						,"Mensagem"=>"Atualizado com sucesso"
+					)
+					,array(
+						"Codigo"=>7	
+						,"Mensagem"=>"Excluído com sucesso"
+					)
+				 );
+	$retorno = array();
+	//Busca pelo erro com o codigo informado de parâmetro
+	foreach($mensagens as $item){
+		if($item["Codigo"] == $codigo){
+			$retorno = $item;
+			break;
+		}
+	}
+	return $retorno;
+}
+?>
---
+++ professor.php
@@ -0,0 +1,124 @@
+<?php
+function ListaProfessores($id){
+	include("conectar.php");
+	
+	$resposta = array();
+	$id = mysqli_real_escape_string($conexao,$id);
+	
+	//Consulta Professores no banco
+	if($id == 0){
+		$query = mysqli_query($conexao,"SELECT idProfessor, Nome FROM Professor") or die(mysqli_error($conexao));
+	}else{
+		$query = mysqli_query($conexao,"SELECT idProfessor, Nome FROM Professor WHERE idProfessor = " .$id) or die(mysqli_error($conexao));
+	}
+	//faz um looping e cria um array com os campos da consulta
+	while($dados = mysqli_fetch_array($query))
+	{
+		$resposta[] = array('idProfessor' => $dados['idProfessor'],
+							'Nome' => utf8_encode($dados['Nome'])); //tira encode quando for publicar no host
+	}
+	return $resposta;
+}
+
+function InsereProfessor(){
+	
+	//Recupera conteudo recebido na request
+	$conteudo = file_get_contents("php://input");
+	$resposta = array();
+	//Verifica se o conteudo foi recebido
+	if(empty($conteudo)){
+		$resposta = mensagens(2);
+	}
+	else{
+		//Converte o json recebido pra array
+		$dados = json_decode($conteudo,true);
+		
+		//Verifica se as infromações esperadas foram recebidas
+		if(!isset($dados["Nome"]))
+		{
+			$resposta = mensagens(3);
+		}
+		else{
+			include("conectar.php");
+			
+			//Evita SQL injection
+			$Nome = mysqli_real_escape_string($conexao,$dados["Nome"]);
+					
+			//Recupera idProfessor para incrementar 1
+			$idProfessor = 1;
+			$query = mysqli_query($conexao, "SELECT idProfessor FROM Professor ORDER BY idProfessor DESC LIMIT 1") or die(mysqli_error($conexao));
+			while($dados = mysqli_fetch_array($query)){
+				$idProfessor = $dados["idProfessor"];
+			}
+			$idProfessor++;
+			
+			//Insere Professor
+			$query = mysqli_query($conexao,"INSERT INTO Professor VALUES(" .$idProfessor .",'" .$Nome ."')") or die(mysqli_error($conexao));
+			$resposta = mensagens(4);
+		}
+	}
+	return $resposta;
+}
+function AtualizaAnimal($id){
+	
+	//Recupera conteudo recebido na request
+	$conteudo = file_get_contents("php://input");
+	$resposta = array();
+	//Verifica se o id foi recebido
+	if($id == 0){
+		$resposta = mensagens(5);
+	}
+	else{
+		//Verifica se o conteudo foi recebido
+		if(empty($conteudo)){
+			$resposta = mensagens(2);
+		}
+		else{
+			//Converte o json recebido pra array
+			$dados = json_decode($conteudo,true);
+			
+			//Verifica se as infromações esperadas foram recebidas
+			if(!isset($dados["Nome"]))
+			{
+				$resposta = mensagens(3);
+			}
+			else
+			{
+				include("conectar.php");
+				
+				//Evita SQL injection
+				$Nome = mysqli_real_escape_string($conexao,$dados["Nome"]);
+						
+				$update = "UPDATE Professor SET Nome = '" .$Nome ."'";
+								
+				
+				//Atualiza Professor no banco
+				$query = mysqli_query($conexao, $update) or die(mysqli_error($conexao));
+				$resposta = mensagens(6);
+			}
+		}
+	}
+	return $resposta;
+}
+function ExcluiProfessor($id){
+	
+	//Recupera conteudo recebido na request
+	$resposta = array();
+	//Verifica se o id foi recebido
+	if($id == 0){
+		$resposta = mensagens(5);
+	}
+	else{
+		include("conectar.php");
+		
+		//Evita SQL injection		
+		$id = mysqli_real_escape_string($conexao,$id);
+		
+		//Exclui Professor
+		$query = mysqli_query($conexao, "DELETE FROM Professor WHERE idProfessor=" .$id) or die(mysqli_error($conexao));
+		
+		$resposta = mensagens(7);
+	}
+	return $resposta;
+}
+?>
