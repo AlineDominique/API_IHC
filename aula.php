2db12b41f7222f5d7e5b606efd8ccc4dbb9996ab
AlineDominique <alinedomi.silva@gmail.com>
2016-12-04 19:01:05 -0200

    Ajustes

--- aula.php
+++ aula.php
@@ -7,9 +7,9 @@ function ListaAulasPorProfessor($id){
 	
 	//Consulta Aula no banco
 	if($id == 0){
-		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala ORDER BY a.Dia ASC") or die(mysqli_error($conexao));
+		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala ORDER BY a.Dia, Professor ASC") or die(mysqli_error($conexao));
 	}else{
-		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala WHERE Professor = '" .$id ."' ORDER BY a.Dia ASC") or die(mysqli_error($conexao));
+		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala WHERE c.idProfessor = " .$id ." ORDER BY a.Dia, Professor ASC") or die(mysqli_error($conexao));
 	}
 	//faz um looping e cria um array com os campos da consulta
 	while($dados = mysqli_fetch_array($query))
@@ -28,17 +28,17 @@ function ListaAulasPorProfessor($id){
 	}
 	return $resposta;
 }
-function ListaAulasPorSala($NRO){
+function ListaAulasPorSala($id){
 	include("conectar.php");
 	
 	$resposta = array();
-	$NRO = mysqli_real_escape_string($conexao,$NRO);
+	$id = mysqli_real_escape_string($conexao,$id);
 	
 	//Consulta Aula no banco
-	if($NRO == 0){
-		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala ORDER BY a.Dia ASC") or die(mysqli_error($conexao));
+	if($id == 0){
+		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala ORDER BY a.Dia, Sala ASC") or die(mysqli_error($conexao));
 	}else{
-		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala WHERE d.Numero = '" .$NRO ."' ORDER BY a.Dia ASC") or die(mysqli_error($conexao));
+		$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala WHERE d.idSala = " .$id ." ORDER BY a.Dia, Sala ASC") or die(mysqli_error($conexao));
 	}
 	//faz um looping e cria um array com os campos da consulta
 	while($dados = mysqli_fetch_array($query))
@@ -57,7 +57,102 @@ function ListaAulasPorSala($NRO){
 	}
 	return $resposta;
 }
-// Testar Código
+
+function PesquisaAulaPorSala(){
+
+	//Recupera conteudo recebido na request
+	$conteudo = file_get_contents("php://input");
+	$resposta = array();
+	//Verifica se o conteudo foi recebido
+	if(empty($conteudo)){
+		$resposta = mensagens(2);
+	}
+	else{
+		//Converte o json recebido pra array
+		$dados = json_decode($conteudo,true);
+		
+		//Verifica se as infromações esperadas foram recebidas
+		if(!isset($dados["Numero"]) || !isset($dados["Dia"]))
+		{
+			$resposta = mensagens(3);
+		}
+		else{
+			include("conectar.php");
+			
+			//Evita SQL injection
+			$Numero = mysqli_real_escape_string($conexao,$dados["Numero"]);
+			$Dia = mysqli_real_escape_string($conexao,$dados["Dia"]);
+			
+			//Consulta Aula no banco
+			$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala WHERE d.Numero = '" .$Numero ."' AND a.Dia = '" .$Dia ."' ORDER BY a.Dia, Sala ASC") or die(mysqli_error($conexao));
+			
+			//faz um looping e cria um array com os campos da consulta
+			while($dados = mysqli_fetch_array($query))
+			{
+				$resposta[] = array('idAula' => $dados['idAula'],
+									'Dia' => $dados['Dia'],
+									'HoraInicio' => $dados['HoraInicio'],
+									'HoraFim' => $dados['HoraFim'],
+									'Semestre' => $dados['Semestre'],
+									'idDisciplina' => $dados['idDisciplina'],
+									'Disciplina' => $dados['Disciplina'],
+									'idProfessor' => $dados['idProfessor'],
+									'Professor' => $dados['Professor'],
+									'idSala' => $dados['idSala'],
+									'Sala' => $dados['Sala']);
+			}
+			return $resposta;
+		}
+	}
+}
+
+function PesquisaAulaPorProfessor(){
+
+	//Recupera conteudo recebido na request
+	$conteudo = file_get_contents("php://input");
+	$resposta = array();
+	//Verifica se o conteudo foi recebido
+	if(empty($conteudo)){
+		$resposta = mensagens(2);
+	}
+	else{
+		//Converte o json recebido pra array
+		$dados = json_decode($conteudo,true);
+		
+		//Verifica se as infromações esperadas foram recebidas
+		if(!isset($dados["Nome"]) || !isset($dados["Dia"]))
+		{
+			$resposta = mensagens(3);
+		}
+		else{
+			include("conectar.php");
+			
+			//Evita SQL injection
+			$Nome = mysqli_real_escape_string($conexao,$dados["Nome"]);
+			$Dia = mysqli_real_escape_string($conexao,$dados["Dia"]);
+			
+			//Consulta Aula no banco
+			$query = mysqli_query($conexao,"SELECT a.idAula, a.Dia, a.HoraInicio, a.HoraFim, a.Semestre, b.idDisciplina, b.Nome as 'Disciplina', c.idProfessor, c.Nome as 'Professor', d.idSala, d.Numero as 'Sala' FROM Aula as a INNER JOIN Disciplina as b on a.idDisciplina = b.idDisciplina INNER JOIN Professor as c on a.idProfessor = c.idProfessor INNER JOIN Sala as d on a.idSala = d.idSala WHERE c.Nome = '" .$Nome ."' AND a.Dia = '" .$Dia ."' ORDER BY a.Dia, Sala ASC") or die(mysqli_error($conexao));
+			
+			//faz um looping e cria um array com os campos da consulta
+			while($dados = mysqli_fetch_array($query))
+			{
+				$resposta[] = array('idAula' => $dados['idAula'],
+									'Dia' => $dados['Dia'],
+									'HoraInicio' => $dados['HoraInicio'],
+									'HoraFim' => $dados['HoraFim'],
+									'Semestre' => $dados['Semestre'],
+									'idDisciplina' => $dados['idDisciplina'],
+									'Disciplina' => $dados['Disciplina'],
+									'idProfessor' => $dados['idProfessor'],
+									'Professor' => $dados['Professor'],
+									'idSala' => $dados['idSala'],
+									'Sala' => $dados['Sala']);
+			}
+			return $resposta;
+		}
+	}
+}
 
 function InsereAula(){
 	
